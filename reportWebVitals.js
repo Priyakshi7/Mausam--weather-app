4d9e565e527eb314b70f7acfa80fa66bd86883bb
Priyakshi Vashista <136305724+Priyakshi7@users.noreply.github.com>
2023-07-25 23:12:37 +0530

    Add files via upload

---
+++ reportWebVitals.js
@@ -0,0 +1,14 @@
+const reportWebVitals = onPerfEntry => {
+    if (onPerfEntry && onPerfEntry instanceof Function) {
+      import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
+        getCLS(onPerfEntry);
+        getFID(onPerfEntry);
+        getFCP(onPerfEntry);
+        getLCP(onPerfEntry);
+        getTTFB(onPerfEntry);
+      });
+    }
+  };
+  // Have webpack 5 set the fallbacks for the resolver for Mui 5 to be false
+
+  export default reportWebVitals;
